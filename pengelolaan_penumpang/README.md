1671e04f021af5aed6587b4c30a672eb1c8b203f
Dandi Sabilli Izzulhaq <45232854+dandiabil@users.noreply.github.com>
2022-02-05 22:38:22 +0700

    few changes on kelola angkot

---
+++ pengelolaan_penumpang/README.md
@@ -0,0 +1,9 @@
+# Color Slider
+
+Simple Angkot Management using HTML, JS, CSS
+
+![contoh program](./assets/kelola-angkot.png)
+
+## Description
+
+This project using the stack concept. How it works? Just type the name and the passanger and then they'll be added to the angkot
--- pengelolaan_penumpang/script.js
+++ pengelolaan_penumpang/script.js
@@ -1,86 +1,66 @@
-//Program pengelolaan penumpang terdiri dari 12 elemen array penumpang
-//ada dua fungsi tambahPenumpang() dan hapusPenumpang()
-//hapusPenumpang()
-//tambahPenumpang(nama penumpang, array penumpang)
+const penumpang = document.querySelectorAll("#penumpang");
+const liPenumpang = Array.from(penumpang).map((p) => p.textContent);
 
-
-// let penumpang = ['Dandi', 'a', 'b', 'c', 'd'];
-// let penumpang = [];
-
-const penumpang = document.querySelectorAll('#penumpang');
-let liPenumpang = [];
-penumpang.forEach(p => liPenumpang.push(p.textContent));
-
-// const btnTambah = document.querySelector('.btnTambah');
-// let inputTambah = document.getElementById('tambah');
-
-const button = document.querySelectorAll('button');
+const button = document.querySelectorAll("button");
 const [btnTambah, btnHapus] = button;
-const input = document.querySelectorAll('input');
+const input = document.querySelectorAll("input");
 const [inputTambah, inputHapus] = input;
 
-btnTambah.addEventListener('click', function(){
-    tambahPenumpang(inputTambah.value, liPenumpang);
-    updateUI();
+btnTambah.addEventListener("click", () => {
+  if (!inputTambah.value) return alert("Nama penumpang tidak boleh kosong!");
+  tambahPenumpang(inputTambah.value, liPenumpang);
+  updateUI();
 });
 
-// const btnHapus = document.querySelector('.btnHapus');
-// let inputHapus = document.getElementById('hapus');
-
-btnHapus.addEventListener('click', function(){
-    hapusPenumpang(inputHapus.value, liPenumpang);
-    updateUI();
+btnHapus.addEventListener("click", () => {
+  if (!inputHapus.value) return alert("Nama penumpang tidak boleh kosong!");
+  hapusPenumpang(inputHapus.value, liPenumpang);
+  updateUI();
 });
 
+const updateUI = () => {
+  penumpang.forEach((p, i) => (p.textContent = liPenumpang[i]));
+  input.forEach((i) => (i.value = ""));
+};
 
-function updateUI(){
-    penumpang.forEach((p, i) => p.textContent = liPenumpang[i]);
-    input.forEach(i => i.value = '');
-}
-
-function tambahPenumpang(namaPenumpang, liPenumpang){
-    if(liPenumpang.length == 0){
-        liPenumpang.push(namaPenumpang);
+const tambahPenumpang = (namaPenumpang, liPenumpang) => {
+  if (liPenumpang.length == 0) {
+    liPenumpang.push(namaPenumpang);
+    return liPenumpang;
+  } else {
+    for (let i = 0; i < liPenumpang.length; i++) {
+      if (liPenumpang[i] === "") {
+        liPenumpang[i] = namaPenumpang;
+        penumpang[i].parentNode.classList.replace("kosong", "isi");
         return liPenumpang;
-    }else{
-        for(var i = 0; i < liPenumpang.length; i++){
-            if(liPenumpang[i] === '(Kosong)'){
-                liPenumpang[i] = namaPenumpang;
-                penumpang[i].classList.add('isi');
-                return liPenumpang;
-            }else if(liPenumpang[i] == namaPenumpang){
-                alert("Penumpang sudah ada didalam mobil");
-                return liPenumpang;
-            }
-            // else if(liPenumpang.length - 1 == i){
-            //     liPenumpang.push(namaPenumpang);
-            //     return liPenumpang;
-            // }
-            else if(i == liPenumpang.length){
-                alert('Angkot sudah penuh');
-                return liPenumpang;
-            }
-        }
-    }
-}
-
-function hapusPenumpang(namaPenumpang, liPenumpang){
-    var adaPenumpang = true;
-    if(liPenumpang.length == 0){
-        alert("Angkot kosong");
+      } else if (liPenumpang[i] == namaPenumpang) {
+        alert("Penumpang sudah ada didalam mobil");
         return liPenumpang;
-    }else{
-        for(var i = 0; i < liPenumpang.length; i++){
-            if(liPenumpang[i] == namaPenumpang){
-                liPenumpang[i] = '(Kosong)';
-                penumpang[i].classList.remove('isi');
-                return liPenumpang;
-            }else{
-                adaPenumpang = false;
-            }
-        }
-        if(!adaPenumpang){
-            alert(namaPenumpang + " tidak ada dalam angkot");
-        }
+      } else if (i == liPenumpang.length - 1) {
+        alert("Angkot sudah penuh");
+        return liPenumpang;
+      }
     }
-}
+  }
+};
+
+const hapusPenumpang = (namaPenumpang, liPenumpang) => {
+  let adaPenumpang = true;
+  if (liPenumpang.length == 0) {
+    alert("Angkot kosong");
+    return liPenumpang;
+  } else {
+    for (let i = 0; i < liPenumpang.length; i++) {
+      if (liPenumpang[i] == namaPenumpang) {
+        liPenumpang[i] = "";
+        penumpang[i].parentNode.classList.replace("isi", "kosong");
+        return liPenumpang;
+      } else {
+        adaPenumpang = false;
+      }
+    }
+    if (!adaPenumpang) {
+      alert(namaPenumpang + " tidak ada dalam angkot");
+    }
+  }
+};
