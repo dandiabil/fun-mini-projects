c8e0ceb32990142f103df25d02adf95c38426070
Dandi Izzulhaq <dandiabil24@gmail.com>
2022-02-10 20:29:06 +0700

    commit changes

--- typing-game/script.js
+++ typing-game/script.js
@@ -1,22 +1,45 @@
-const sentences = document.getElementById("sentences").innerText.split(" ");
-const inputWord = document.getElementById("word");
+const sentencesDisplay = document.getElementById("sentences");
+const inputWord = document.getElementById("input-word");
+const sentences = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae dolorem delectus rerum veritatis amet eveniet ipsum necessitatibus voluptatibus est eligendi"
+const btn = document.getElementById('btn');
+let sentencesArray = [];
 
-inputWord.addEventListener("keypress", (e) => {
-  let guess = false;
+let i = 0;
 
-  if (e.key === " ") {
-    guess = check(e.target.value, i);
-    if (guess) {
-      i++;
+const start = () => {
+  sentencesArray = sentences.split(' ')
+  sentencesArray.map((word, i) => {
+    let display = '';
+    if(i === 0) sentencesDisplay.innerHTML += `<span class="word selected-word">${word}</span> `;
+    else{
+      display = `<span class="word">${word}</span> `;
+      sentencesDisplay.innerHTML += display;
     }
-    console.log(guess, i);
+  })
+  inputWord.focus();
+}
+
+const reset = () => {
+  sentencesDisplay.innerHTML = ''
+  i = 0;
+}
+
+
+inputWord.addEventListener("input", (e) => {
+  const word = document.querySelectorAll('.word')
+  if (e.target.value === sentencesArray[i]) {
+    word[i].classList.remove('selected-word')
+    i++;
     e.target.value = null;
   }
+  word[i].classList.add('selected-word')
 });
 
-const check = (word, i) => {
-  if (i === sentences.length - 1) {
-    if (sentences[i] === word) return true;
-    return false;
+btn.addEventListener('click', (e) => {
+  if(e.target.innerText === 'Start') {
+    e.target.innerText = 'Reset'
+    return start();
   }
-};
+  reset()
+  e.target.innerText = 'Start'
+})
