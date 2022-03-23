42dc1fd30d5ba1e8c27c24eb991a9338bc1f13f4
Dandi Izzulhaq <dandiabil24@gmail.com>
2022-03-23 11:45:30 +0700

    change input listener to keyup

--- typing-game/script.js
+++ typing-game/script.js
@@ -1,45 +1,48 @@
 const sentencesDisplay = document.getElementById("sentences");
 const inputWord = document.getElementById("input-word");
-const sentences = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae dolorem delectus rerum veritatis amet eveniet ipsum necessitatibus voluptatibus est eligendi"
-const btn = document.getElementById('btn');
+const sentences =
+  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae dolorem delectus rerum veritatis amet eveniet ipsum necessitatibus voluptatibus est eligendi";
+const btn = document.getElementById("btn");
 let sentencesArray = [];
 
 let i = 0;
 
 const start = () => {
-  sentencesArray = sentences.split(' ')
+  sentencesArray = sentences.split(" ");
   sentencesArray.map((word, i) => {
-    let display = '';
-    if(i === 0) sentencesDisplay.innerHTML += `<span class="word selected-word">${word}</span> `;
-    else{
+    let display = "";
+    if (i === 0)
+      sentencesDisplay.innerHTML += `<span class="word selected-word">${word}</span> `;
+    else {
       display = `<span class="word">${word}</span> `;
       sentencesDisplay.innerHTML += display;
     }
-  })
+  });
   inputWord.focus();
-}
+};
 
 const reset = () => {
-  sentencesDisplay.innerHTML = ''
+  sentencesDisplay.innerHTML = "";
   i = 0;
-}
+};
 
-
-inputWord.addEventListener("input", (e) => {
-  const word = document.querySelectorAll('.word')
-  if (e.target.value === sentencesArray[i]) {
-    word[i].classList.remove('selected-word')
-    i++;
-    e.target.value = null;
+inputWord.addEventListener("keyup", (e) => {
+  const word = document.querySelectorAll(".word");
+  if (e.key === " ") {
+    if (e.target.value.trim() === sentencesArray[i]) {
+      word[i].classList.remove("selected-word");
+      i++;
+      e.target.value = null;
+    }
   }
-  word[i].classList.add('selected-word')
+  word[i].classList.add("selected-word");
 });
 
-btn.addEventListener('click', (e) => {
-  if(e.target.innerText === 'Start') {
-    e.target.innerText = 'Reset'
+btn.addEventListener("click", (e) => {
+  if (e.target.innerText === "Start") {
+    e.target.innerText = "Reset";
     return start();
   }
-  reset()
-  e.target.innerText = 'Start'
-})
+  reset();
+  e.target.innerText = "Start";
+});
